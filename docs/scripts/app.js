250877d3bde224e0f9a2193ca220441930c45453
Drew Devero <deverobelfon@gmail.com>
2020-12-09 13:08:10 -0500

    added more numerical functionality

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -4,3 +4,23 @@
 // when one side has no more beads (all pOne OR all pTwo === 0) remaining beads in player mini pots goes into player with remaining beads capture pot (player capture pot += remaining beads)
 // Highest count of player capture pot beads wins.
 // Game resets
+
+$(() => console.log("hello", $(".miniPots").length));
+$("form").on("submit", (e) => {
+    e.preventDefault();
+})
+
+// function applys to ability to click a button and make that individual button value equal to zero
+    /* $(".miniPots").click(()=> {
+        $("input").val("0");
+    }); */
+
+function applyZero(toZero) {
+    $("input[type=submit]").eq(toZero).click(()=> {
+            $("input[type=submit]").eq(toZero).val("0");
+    })
+    if (toZero < $("input[type=submit]").length) {
+        applyZero(toZero+1);
+    }
+}
+applyZero(0);
